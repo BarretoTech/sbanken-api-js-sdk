8ea940a9f4a94823bbb9976251730c035af53242
rhuanbarreto <rhuanbarreto@users.noreply.github.com>
2023-05-28 15:06:59 +0000

    Updated OpenAPI spec

---
+++ clients/api/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountsApi';
+export * from './CardsApi';
+export * from './CustomersApi';
+export * from './MailboxApi';
+export * from './PaymentsApi';
+export * from './StandingOrdersApi';
+export * from './TransactionsApi';
+export * from './TransfersApi';
